82e3327603bb4a3c047799a7dd3e87d7791ce80f
TheGreatRambler <aehart684351@gmail.com>
2019-11-10 23:16:28 +0000

    Rudimentary resizing accomplished

--- ui.hpp
+++ ui.hpp
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <switch.h>
 #include <stdlib.h>
+#include <NvPipe.h>
+// This is needed for image resizing
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "ThirdParty/stb_image_resize.h"
 
 class UI() {
 	private:
@@ -10,6 +14,17 @@ class UI() {
 	// I'm not quite sure why it is that, but it is
 	constexpr u8* Vbuf = aligned_alloc(0x1000, 0x32000);
 	Service grcdVideo;
+	// NVidia H264 decoder
+	NvPipe* nvidiaDecoder;
+	// Dimensions of the screen
+	constexpr int width = 1280;
+	constexpr int height = 720;
+	// Dimensions of the game window in the corner
+	constexpr int gameWidth = 960;
+	constexpr int gameHeight = 540;
+	// RGBA buffers
+	u8* inputRgbaBuffer = malloc(width * height * 4);
+	u8* outputRgbaBuffer = malloc(gameWidth * gameHeight * 4);
 
 // Borrowed from sysDVR
 Result grcdServiceOpen(Service* out) {
@@ -120,9 +135,12 @@ Result _grcCmdNoIO(Service* srv, u64 cmd_id) {
 		if (R_FAILED(rc)) fatalSimple(rc);
 		rc = grcdServiceBegin(&grcdVideo);
 		if (R_FAILED(rc)) fatalSimple(rc);
+		// Create Nvenc decoder
+		nvidiaDecoder = NvPipe_CreateDecoder(NVPIPE_RGBA32, NVPIPE_H264, width, height);
 	}
 
 	void getCurrentFrame() {
+		// This function produces H264 frames in Vbuf with a size of 1280 x 720
 		// I'm not exactly sure what this is, but it's always 0
 		u32 unk = 0;
 		u32 VOutSz = 0;
@@ -138,12 +156,22 @@ Result _grcCmdNoIO(Service* srv, u64 cmd_id) {
 			VOutSz = 0;
 			if (fails++ > 8 && !IsThreadRunning) break;
 		}
+		// Decode the H264 frame with Nvenc, hardware decoding
+		NvPipe_Decode(nvidiaDecoder, Vbuf, sizeof(Vbuf), inputRgbaBuffer, width, height);
+		// Put the RGBA data into the buffer
+		// Assuming it's 4 bytes per pixel
+		stbir_resize_uint8_srgb(inputRgbaBuffer, width, height, 0, outputRgbaBuffer, gameWidth, gameHeight, 0, 4, STBIR_FLAG_ALPHA_PREMULTIPLIED, 0);
+		// At this point, an RGBA buffer of the resized game window is in `outputRgbaBuffer`
 	}
 
 	~NxTASUI() {
 		// Stop service
 		grcdServiceClose(&grcdVideo);
-		// Free the video buffer
+		// Free the video buffers, both H264 and raw RGBA
 		free(Vbuf);
+		free(inputRgbaBuffer);
+		free(outputRgbaBuffer);
+		// Close the Nvidia decoder
+		NvPipe_Destroy(decoder);
 	}
 }
