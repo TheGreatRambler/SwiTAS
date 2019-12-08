226d335d5b640871eead88655e5ca80c3529ee17
TheGreatRambler <aehart684351@gmail.com>
2019-12-08 05:46:08 +0000

    Add bottom input grid with changing
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

--- pc_application/source/bottomUI.hpp
+++ pc_application/source/bottomUI.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
-#include <gtkmm.h>
 #include <cairomm/context.h>
 #include <glibmm/main.h>
+#include <gtkmm.h>
 #include <map>
+#include <utility>
 
 #include "buttonData.hpp"
+#include "dataProcessing.hpp"
 
 // Location
 struct Location {
-	int x;
-	int y;
+  int x;
+  int y;
 }
 
 // https://www.tablesgenerator.com/text_tables
@@ -24,91 +26,125 @@ struct Location {
 //   +----+---+----+----+---+--+---+---+----+---+---+
 // 3 |    | v |    | RS |   |  |   |   |    | B |   |
 //   +----+---+----+----+---+--+---+---+----+---+---+
-std::map<Btn, Location> KeyLocs {
-    {Btn::A, {10, 2}},
-    {Btn::B, {9, 3}},
-    {Btn::X, {9, 1}},
-    {Btn::Y, {8, 2}},
-    {Btn::L, {3, 0}},
-    {Btn::R, {7, 0}},
-    {Btn::ZL, {2, 0}},
-    {Btn::ZR, {8, 0}},
-    {Btn::DUP, {1, 1}},
-    {Btn::DDOWN, {1, 3}},
-    {Btn::DLEFT, {0, 2}},
-    {Btn::DRIGHT, {2, 2}},
-    {Btn::PLUS, {7, 1}},
-    {Btn::MINUS, {3, 1}},
-    {Btn::HOME, {6, 2}},
-    {Btn::CAPT, {4, 2}},
-    {Btn::LS, {0, 0}},
-    {Btn::RS, {3, 3}}, 
-};
+std::map<Btn, Location>
+    KeyLocs{
+        {Btn::A, {10, 2}},    {Btn::B, {9, 3}},     {Btn::X, {9, 1}},
+        {Btn::Y, {8, 2}},     {Btn::L, {3, 0}},     {Btn::R, {7, 0}},
+        {Btn::ZL, {2, 0}},    {Btn::ZR, {8, 0}},    {Btn::DUP, {1, 1}},
+        {Btn::DDOWN, {1, 3}}, {Btn::DLEFT, {0, 2}}, {Btn::DRIGHT, {2, 2}},
+        {Btn::PLUS, {7, 1}},  {Btn::MINUS, {3, 1}}, {Btn::HOME, {6, 2}},
+        {Btn::CAPT, {4, 2}},  {Btn::LS, {0, 0}},    {Btn::RS, {3, 3}},
+    };
 
 // Canvas that the joystick viewer is drawn to
 // https://developer.gnome.org/gtkmm-tutorial/stable/sec-drawing-clock-example.html.en
 class JoystickCanvas : public Gtk::DrawingArea {
-    public:
-    JoystickCanvas() {
-        // Don't know why this is needed
-        // Force a redraw every 30 milliseconds
-        Glib::signal_timeout().connect(sigc::mem_fun(*this, &JoystickCanvas::on_timeout), 30);
+public:
+  JoystickCanvas() {
+    // Don't know why this is needed
+    // Force a redraw every 30 milliseconds
+    Glib::signal_timeout().connect(
+        sigc::mem_fun(*this, &JoystickCanvas::on_timeout), 30);
+  }
+
+protected:
+  // Override default signal handler:
+  bool on_draw(const Cairo::RefPtr<Cairo::Context> &cr) override {
+    Gtk::Allocation allocation = get_allocation();
+    const int width = allocation.get_width();
+    const int height = allocation.get_height();
+
+    // scale to unit square and translate (0, 0) to be (0.5, 0.5), i.e.
+    // the center of the window
+    cr->scale(width, height);
+    cr->translate(0.5, 0.5);
+
+    // TODO do the drawing
+  }
+
+  bool on_timeout() {
+    // Apparently this is needed
+    // force our program to redraw the entire clock.
+    auto win = get_window();
+    if (win) {
+      Gdk::Rectangle r(0, 0, get_allocation().get_width(),
+                       get_allocation().get_height());
+      // Invalidate the canvas so it is drawn again
+      win->invalidate_rect(r, false);
     }
-
-    protected:
-    //Override default signal handler:
-    bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override {
-        Gtk::Allocation allocation = get_allocation();
-        const int width = allocation.get_width();
-        const int height = allocation.get_height();
-
-        // scale to unit square and translate (0, 0) to be (0.5, 0.5), i.e.
-        // the center of the window
-        cr->scale(width, height);
-        cr->translate(0.5, 0.5);
-
-        // TODO do the drawing
-    }
-
-    bool on_timeout() {
-        // Apparently this is needed
-        // force our program to redraw the entire clock.
-        auto win = get_window();
-        if (win) {
-            Gdk::Rectangle r(0, 0, get_allocation().get_width(), get_allocation().get_height());
-            // Invalidate the canvas so it is drawn again
-            win->invalidate_rect(r, false);
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
 class BottomUI {
-    private:
-    // Both joystick drawing areas
-    JoystickCanvas leftJoystick;
-    JoystickCanvas rightJoystick;
+private:
+  // Both joystick drawing areas
+  JoystickCanvas leftJoystick;
+  JoystickCanvas rightJoystick;
 
-    // Grid containing the button viewer
-    Gtk::Grid buttonViewer;
+  // Input instance to get inputs and such
+  DataProcessing *inputInstance;
 
-    // The images displayed (just here to keep a reference)
-    std::vector<Glib::RefPtr<Gtk::Image>> buttonImages;
+  // Grid containing the button viewer
+  Gtk::Grid buttonViewer;
 
-    public:
-    BottomUI() {
-        // TODO set up joysticks
-        // Add grid of buttons
-        for (auto const& button : KeyLocs) {
-            Gtk::Image* image = new Gtk::image(buttonMapping[button.first]->bottomInputViewIcon);
-            buttonImages.push_back(image);
-            buttonViewer.attach(*image, button.second.x, button.second.y);
-        }
+  // The images displayed (just here to keep a reference)
+  std::map<Btn, Gtk::image *> onImages;
+  std::map<Btn, Gtk::image *> offImages;
+
+public:
+  BottomUI() {
+    // TODO set up joysticks
+    // Add grid of buttons
+    for (auto const &button : KeyLocs) {
+      // Create on images and add them
+      Gtk::Image *onImage =
+          new Gtk::image(buttonMapping[button.first]->bottomInputViewIcon);
+      onImages.insert(std::pair<Btn, Gtk::image *>(button.first, onImage));
+      // Create off images and add them
+      Gtk::Image *offImage =
+          new Gtk::image(buttonMapping[button.first]->offIcon);
+      offImages.insert(std::pair<Btn, Gtk::image *>(button.first, offImage));
+
+      // Designate the off image as the default
+      buttonViewer.attach(*offImage, button.second.x, button.second.y);
     }
+  }
 
-    void addToGrid(Gtk::Grid* theGrid) {
-        theGrid->attach(leftJoystick, 0, 0);
-        theGrid->attach(rightJoystick, 1, 0);
-        theGrid->attach(buttonViewer, 2, 0);
+  void setInputInstance(DataProcessing *input) { inputInstance = input; }
+
+  void setIconState(Btn button, bool state) {
+    bool currentState = inputInstance->getButtonState(button);
+    // Only update if there is a need to update
+    if (currentState != state) {
+      if (currentState) {
+        buttonViewer.remove(*onImages[button]);
+        buttonViewer.attach(*offImages[button], KeyLocs[button].x,
+                            KeyLocs[button].y);
+      } else {
+        buttonViewer.remove(*offImages[button]);
+        buttonViewer.attach(*onImages[button], KeyLocs[button].x,
+                            KeyLocs[button].y);
+      }
+
+      // Set value in input instance
+      inputInstance->setButtonState(button, state);
     }
+  }
+
+  void addToGrid(Gtk::Grid *theGrid) {
+    theGrid->attach(leftJoystick, 0, 0);
+    theGrid->attach(rightJoystick, 1, 0);
+    theGrid->attach(buttonViewer, 2, 0);
+  }
+
+  ~BottomUI() {
+    // Deallocate all the images
+    for (auto const &image : onImages) {
+      free(image.second);
+    }
+    for (auto const &image : offImages) {
+      free(image.second);
+    }
+  }
 }
--- pc_application/source/dataProcessing.hpp
+++ pc_application/source/dataProcessing.hpp
@@ -1,99 +1,100 @@
 #pragma once
 
+#include <bitset>
 #include <cstdint>
 #include <cstdlib>
-#include <bitset>
+#include <gtkmm.h>
+#include <map>
+#include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
-#include <map>
-#include <tuple>
-#include <string>
-#include <gtkmm.h>
 
 #include "buttonData.hpp"
 
 class InputColumns : public Gtk::TreeModelColumnRecord {
-    public:
-    Gtk::TreeModelColumn<uint32_t> frameNum;
-    // All the buttons are stored inside of buttonMapping
-    // https://developer.gnome.org/gtkmm-tutorial/stable/sec-treeview-examples.html.en
+public:
+  Gtk::TreeModelColumn<uint32_t> frameNum;
+  // All the buttons are stored inside of buttonMapping
+  // https://developer.gnome.org/gtkmm-tutorial/stable/sec-treeview-examples.html.en
 
-    InputColumns() {
-        add(frameNum);
-        // Loop through the buttons and add them
-        for (auto const& button : buttonMapping) {
-            // Gets pointer from tuple
-            Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>* thisIcon = button.second->columnIcon;
-            // Add to the columns themselves (gives value, not pointer)
-            add(*thisIcon);
-        }
-
+  InputColumns() {
+    add(frameNum);
+    // Loop through the buttons and add them
+    for (auto const &button : buttonMapping) {
+      // Gets pointer from tuple
+      Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> *thisIcon =
+          button.second->columnIcon;
+      // Add to the columns themselves (gives value, not pointer)
+      add(*thisIcon);
     }
+  }
 }
 
 class DataProcessing {
-    private:
-    // Vector storing ALL inputs
-    // Shared pointer so everything is nice
-    std::vector<std::shared_ptr<ControllerData>> inputsList;
-    // Current input
-    std::shared_ptr<ControllerData> currentData;
-    // Current frame
-    uint32_t currentFrame;
-    // Tree data storing the controller stuffs
-    Glib::RefPtr<Gtk::ListStore> controllerListStore;
-    // Stores the columns for the above list store
-    InputColumns inputColumns;
-    // Tree view viewed in the UI
-    Gtk::TreeView treeView;
+private:
+  // Vector storing ALL inputs
+  // Shared pointer so everything is nice
+  std::vector<std::shared_ptr<ControllerData>> inputsList;
+  // Current input
+  std::shared_ptr<ControllerData> currentData;
+  // Current frame
+  uint32_t currentFrame;
+  // Tree data storing the controller stuffs
+  Glib::RefPtr<Gtk::ListStore> controllerListStore;
+  // Stores the columns for the above list store
+  InputColumns inputColumns;
+  // Tree view viewed in the UI
+  Gtk::TreeView treeView;
 
-    public:
-    DataProcessing() {
-        //Add the list store from the columns
-        controllerListStore = Gtk::ListStore::create(inputColumns);
-        // Set this tree view to this model
-        treeView.set_model(controllerListStore);
-        // Add all the columns, this somehow wasn't done already
-        treeView.append_column("Frame", inputColumns.frameNum);
-        // Loop through buttons and add all of them
-        for (auto const& thisButton : buttonMapping) {
-            // Append with the string specified by Button Mapping
-            // Get value of columnIcon, not pointer
-            treeView.append_column(thisButton.second->viewName, *thisButton.second->columnIcon);
-        }
-        // Add this first frame
-        addNewFrame(true);
+public:
+  DataProcessing() {
+    // Add the list store from the columns
+    controllerListStore = Gtk::ListStore::create(inputColumns);
+    // Set this tree view to this model
+    treeView.set_model(controllerListStore);
+    // Add all the columns, this somehow wasn't done already
+    treeView.append_column("Frame", inputColumns.frameNum);
+    // Loop through buttons and add all of them
+    for (auto const &thisButton : buttonMapping) {
+      // Append with the string specified by Button Mapping
+      // Get value of columnIcon, not pointer
+      treeView.append_column(thisButton.second->viewName,
+                             *thisButton.second->columnIcon);
     }
+    // Add this first frame
+    addNewFrame(true);
+  }
 
-    bool getButtonState(Btn button) {
-        // Get value from the bitset
-        return currentData->buttons.test(button);
+  bool getButtonState(Btn button) {
+    // Get value from the bitset
+    return currentData->buttons.test(button);
+  }
+
+  void setButtonState(Btn button, bool state) {
+    // If state is true, on, else off
+    return currentData->buttons.set(button, state);
+  }
+
+  void setCurrentFrame(uint32_t frameNum) {
+    // Must be a frame that has already been written, else, raise error
+    if (frameNum < inputsList.size() && frameNum > -1) {
+      // Set the current frame to this frame
+      // Shared pointer so this can be done
+      currentData = inputsList[frameNum];
     }
+  }
 
-    void setButtonState(Btn button, bool state) {
-        // If state is true, on, else off
-        return currentData->buttons.set(button, state);
+  void addNewFrame(bool isFirstFrame = false) {
+    if (!isFirstFrame) {
+      // On the first frame, it is already set right
+      currentFrame++;
     }
+    // Will overwrite previous frame if need be
+    currentData = std::make_shared<ControllerData>();
+    // Add this to the vector
+    inputsList.push_back(currentData);
+  }
 
-    void setCurrentFrame(uint32_t frameNum) {
-        // Must be a frame that has already been written, else, raise error
-        if (frameNum < inputsList.size() && frameNum > -1) {
-            // Set the current frame to this frame
-            // Shared pointer so this can be done
-            currentData = inputsList[frameNum];
-        }
-    }
-
-    void addNewFrame(bool isFirstFrame = false) {
-        if (!isFirstFrame) {
-            // On the first frame, it is already set right
-            currentFrame++;
-        }
-        // Will overwrite previous frame if need be
-        currentData = std::make_shared<ControllerData>();
-        // Add this to the vector
-        inputsList.push_back(currentData);
-    }
-
-    ~DataProcessing() {}
+  ~DataProcessing() {}
 }
