13be3f51a3992eabd4c5f8a075c220263e0c0d69
TheGreatRambler <aehart684351@gmail.com>
2019-12-10 17:38:27 +0000

    Add row handing code
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

--- pc_application/source/buttonData.hpp
+++ pc_application/source/buttonData.hpp
@@ -8,83 +8,82 @@
 // Buttons enum (coencides with the index of the bit in the input struct)
 // Also used to identify the button everywhere else in the program
 enum Btn {
-  A,
-  B,
-  X,
-  Y,
-  L,
-  R,
-  ZL,
-  ZR,
-  SL,
-  SR,
-  DUP,
-  DDOWN,
-  DLEFT,
-  DRIGHT,
-  PLUS,
-  MINUS,
-  HOME,
-  CAPT,
-  LS,
-  RS,
+	A,
+	B,
+	X,
+	Y,
+	L,
+	R,
+	ZL,
+	ZR,
+	SL,
+	SR,
+	DUP,
+	DDOWN,
+	DLEFT,
+	DRIGHT,
+	PLUS,
+	MINUS,
+	HOME,
+	CAPT,
+	LS,
+	RS,
 };
 
 // Controller data that will be packed into the array and will be recieved from
 // the switch
 struct ControllerData {
-  // This controller's index
-  uint8_t index;
-  // Button data (stored as a bitset because it will be serialized better later)
-  // 20 buttons
-  // This storage can be sacrificed for the simple reason that the other data
-  // already takes up so much space
-  std::bitset<20> buttons;
-  // Joystick values
-  int16_t LS_X = 0;
-  int16_t LS_Y = 0;
-  int16_t RS_X = 0;
-  int16_t RS_Y = 0;
-  // Gyroscope and Accelerometer data (when it is implemented)
-  int16_t ACCEL_X = 0;
-  int16_t ACCEL_Y = 0;
-  int16_t ACCEL_Z = 0;
-  int16_t GYRO_1 = 0;
-  int16_t GYRO_2 = 0;
-  int16_t GYRO_3 = 0;
+	// This controller's index
+	uint8_t index;
+	// Button data (stored as a bitset because it will be serialized better later)
+	// 20 buttons
+	// This storage can be sacrificed for the simple reason that the other data
+	// already takes up so much space
+	std::bitset<20> buttons;
+	// Joystick values
+	int16_t LS_X = 0;
+	int16_t LS_Y = 0;
+	int16_t RS_X = 0;
+	int16_t RS_Y = 0;
+	// Gyroscope and Accelerometer data (when it is implemented)
+	int16_t ACCEL_X = 0;
+	int16_t ACCEL_Y = 0;
+	int16_t ACCEL_Z = 0;
+	int16_t GYRO_1  = 0;
+	int16_t GYRO_2  = 0;
+	int16_t GYRO_3  = 0;
 }
 
 // Struct containing button info
 struct ButtonInfo {
-  Glib::ustring scriptName;
-  Glib::ustring viewName;
-  Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> *columnIcon;
-  Glib::RefPtr<Gdk::Pixbuf> onIcon;
-  Glib::RefPtr<Gdk::Pixbuf> offIcon;
+	Glib::ustring scriptName;
+	Glib::ustring viewName;
+	Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>* columnIcon;
+	Glib::RefPtr<Gdk::Pixbuf> onIcon;
+	Glib::RefPtr<Gdk::Pixbuf> offIcon;
 }
 
-Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> *
-getNewColumn() {
-  return new Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>();
+Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>*
+getNewColumn () {
+	return new Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> ();
 }
 
-Glib::RefPtr<Gdk::Pixbuf> getNewIcon(std::string name) {
-  // https://stackoverflow.com/questions/5894344/gtkmm-how-to-put-a-pixbuf-in-a-treeview
-  return Gdk::Pixbuf::create_from_file("/usr/share/icons/gnome/22x22/apps/" +
-                                       name + ".png");
+Glib::RefPtr<Gdk::Pixbuf> getNewIcon (std::string name) {
+	// https://stackoverflow.com/questions/5894344/gtkmm-how-to-put-a-pixbuf-in-a-treeview
+	return Gdk::Pixbuf::create_from_file ("/usr/share/icons/gnome/22x22/apps/" + name + ".png");
 }
 
-ButtonInfo *gBI(Glib::ustring scriptName, Glib::ustring viewName,
-                Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> *columnIcon,
-                Glib::RefPtr<Gdk::Pixbuf> viewIcon,
-                Glib::RefPtr<Gdk::Pixbuf> offIcon) {
-  ButtonInfo *thisButtonInfo = new ButtonInfo();
-  thisButtonInfo->scriptName = scriptName;
-  thisButtonInfo->viewName = viewName;
-  thisButtonInfo->columnIcon = columnIcon;
-  thisButtonInfo->onIcon = viewIcon;
-  thisButtonInfo->offIcon = offIcon;
-  return thisButtonInfo;
+ButtonInfo* gBI (Glib::ustring scriptName, Glib::ustring viewName,
+	Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>* columnIcon,
+	Glib::RefPtr<Gdk::Pixbuf> viewIcon,
+	Glib::RefPtr<Gdk::Pixbuf> offIcon) {
+	ButtonInfo* thisButtonInfo = new ButtonInfo ();
+	thisButtonInfo->scriptName = scriptName;
+	thisButtonInfo->viewName   = viewName;
+	thisButtonInfo->columnIcon = columnIcon;
+	thisButtonInfo->onIcon     = viewIcon;
+	thisButtonInfo->offIcon    = offIcon;
+	return thisButtonInfo;
 }
 
 // Array including all button information
@@ -92,45 +91,45 @@ ButtonInfo *gBI(Glib::ustring scriptName, Glib::ustring viewName,
 // listview object, Button printing pixbuf when on and when off This info is
 // contained in a struct
 // TODO finish these
-std::map<Btn, ButtonInfo *> buttonMapping{
-    {Btn::A,
-     gBI("KEY_A", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::B,
-     gBI("KEY_B", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::X,
-     gBI("KEY_X", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::Y,
-     gBI("KEY_Y", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::L,
-     gBI("KEY_L", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::R,
-     gBI("KEY_R", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::ZL,
-     gBI("KEY_ZL", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::ZR,
-     gBI("KEY_ZR", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::SL,
-     gBI("KEY_SL", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::SR,
-     gBI("KEY_SR", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::DUP, gBI("KEY_DUP", "A Button", getNewColumn(), getNewIcon(""),
-                   getNewIcon(""))},
-    {Btn::DDOWN, gBI("KEY_DDOWN", "A Button", getNewColumn(), getNewIcon(""),
-                     getNewIcon(""))},
-    {Btn::DLEFT, gBI("KEY_DLEFT", "A Button", getNewColumn(), getNewIcon(""),
-                     getNewIcon(""))},
-    {Btn::DRIGHT, gBI("KEY_DRIGHT", "A Button", getNewColumn(), getNewIcon(""),
-                      getNewIcon(""))},
-    {Btn::PLUS, gBI("KEY_PLUS", "A Button", getNewColumn(), getNewIcon(""),
-                    getNewIcon(""))},
-    {Btn::MINUS, gBI("KEY_MINUS", "A Button", getNewColumn(), getNewIcon(""),
-                     getNewIcon(""))},
-    {Btn::HOME, gBI("KEY_HOME", "A Button", getNewColumn(), getNewIcon(""),
-                    getNewIcon(""))},
-    {Btn::CAPT, gBI("KEY_CAPT", "A Button", getNewColumn(), getNewIcon(""),
-                    getNewIcon(""))},
-    {Btn::LS,
-     gBI("KEY_LS", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
-    {Btn::RS,
-     gBI("KEY_RS", "A Button", getNewColumn(), getNewIcon(""), getNewIcon(""))},
+std::map<Btn, ButtonInfo*> buttonMapping {
+	{ Btn::A,
+		gBI ("KEY_A", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::B,
+		gBI ("KEY_B", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::X,
+		gBI ("KEY_X", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::Y,
+		gBI ("KEY_Y", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::L,
+		gBI ("KEY_L", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::R,
+		gBI ("KEY_R", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::ZL,
+		gBI ("KEY_ZL", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::ZR,
+		gBI ("KEY_ZR", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::SL,
+		gBI ("KEY_SL", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::SR,
+		gBI ("KEY_SR", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::DUP, gBI ("KEY_DUP", "A Button", getNewColumn (), getNewIcon (""),
+					getNewIcon ("")) },
+	{ Btn::DDOWN, gBI ("KEY_DDOWN", "A Button", getNewColumn (), getNewIcon (""),
+					  getNewIcon ("")) },
+	{ Btn::DLEFT, gBI ("KEY_DLEFT", "A Button", getNewColumn (), getNewIcon (""),
+					  getNewIcon ("")) },
+	{ Btn::DRIGHT, gBI ("KEY_DRIGHT", "A Button", getNewColumn (), getNewIcon (""),
+					   getNewIcon ("")) },
+	{ Btn::PLUS, gBI ("KEY_PLUS", "A Button", getNewColumn (), getNewIcon (""),
+					 getNewIcon ("")) },
+	{ Btn::MINUS, gBI ("KEY_MINUS", "A Button", getNewColumn (), getNewIcon (""),
+					  getNewIcon ("")) },
+	{ Btn::HOME, gBI ("KEY_HOME", "A Button", getNewColumn (), getNewIcon (""),
+					 getNewIcon ("")) },
+	{ Btn::CAPT, gBI ("KEY_CAPT", "A Button", getNewColumn (), getNewIcon (""),
+					 getNewIcon ("")) },
+	{ Btn::LS,
+		gBI ("KEY_LS", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
+	{ Btn::RS,
+		gBI ("KEY_RS", "A Button", getNewColumn (), getNewIcon (""), getNewIcon ("")) },
 };
--- pc_application/source/dataProcessing.hpp
+++ pc_application/source/dataProcessing.hpp
@@ -14,100 +14,108 @@
 
 class InputColumns : public Gtk::TreeModelColumnRecord {
 public:
-  Gtk::TreeModelColumn<uint32_t> frameNum;
-  // All the buttons are stored inside of buttonMapping
-  // https://developer.gnome.org/gtkmm-tutorial/stable/sec-treeview-examples.html.en\
+	Gtk::TreeModelColumn<uint32_t> frameNum;
+	// All the buttons are stored inside of buttonMapping
+	// https://developer.gnome.org/gtkmm-tutorial/stable/sec-treeview-examples.html.en
+	std::map<Btn, Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>*> buttonPixbufs;
 
-  InputColumns() {
-    add(frameNum);
-    // Loop through the buttons and add them
-    for (auto const &button : buttonMapping) {
-      // Gets pointer from tuple
-      Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> *thisIcon =
-          button.second->columnIcon;
-      // Add to the columns themselves (gives value, not pointer)
-      add(*thisIcon);
-    }
-  }
+	InputColumns () {
+		add (frameNum);
+		// Loop through the buttons and add them
+		for (auto const& button : buttonMapping) {
+			// Gets pointer from tuple
+			Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>>* thisIcon = button.second->columnIcon;
+			// Add to map for later
+			buttonPixbufs[button.first] = thisIcon;
+			// Add to the columns themselves (gives value, not pointer)
+			add (*thisIcon);
+		}
+	}
 }
 
 class DataProcessing {
 private:
-  // Vector storing ALL inputs
-  // Shared pointer so everything is nice
-  std::vector<std::shared_ptr<ControllerData>> inputsList;
-  // Current input
-  std::shared_ptr<ControllerData> currentData;
-  // Current frame
-  uint32_t currentFrame;
-  // Tree data storing the controller stuffs
-  Glib::RefPtr<Gtk::ListStore> controllerListStore;
-  // Stores the columns for the above list store
-  InputColumns inputColumns;
-  // Tree view viewed in the UI
-  Gtk::TreeView treeView;
+	// Vector storing ALL inputs
+	// Shared pointer so everything is nice
+	std::vector<std::shared_ptr<ControllerData>> inputsList;
+	// Current input
+	std::shared_ptr<ControllerData> currentData;
+	// Current frame
+	uint32_t currentFrame;
+	// Tree data storing the controller stuffs
+	Glib::RefPtr<Gtk::ListStore> controllerListStore;
+	// Stores the columns for the above list store
+	InputColumns inputColumns;
+	// Tree view viewed in the UI
+	Gtk::TreeView treeView;
 
 public:
-  DataProcessing() {
-    // Add the list store from the columns
-    controllerListStore = Gtk::ListStore::create(inputColumns);
-    // Set this tree view to this model
-    treeView.set_model(controllerListStore);
-    // Add all the columns, this somehow wasn't done already
-    treeView.append_column("Frame", inputColumns.frameNum);
-    // Loop through buttons and add all of them
-    for (auto const &thisButton : buttonMapping) {
-      // Append with the string specified by Button Mapping
-      // Get value of columnIcon, not pointer
-      treeView.append_column(thisButton.second->viewName,
-                             *thisButton.second->columnIcon);
-    }
-    // Add this first frame
-    addNewFrame(true);
-  }
+	DataProcessing () {
+		// Add the list store from the columns
+		controllerListStore = Gtk::ListStore::create (inputColumns);
+		// Set this tree view to this model
+		treeView.set_model (controllerListStore);
+		// Add all the columns, this somehow wasn't done already
+		treeView.append_column ("Frame", inputColumns.frameNum);
+		// Loop through buttons and add all of them
+		for (auto const& thisButton : buttonMapping) {
+			// Append with the string specified by Button Mapping
+			// Get value of columnIcon, not pointer
+			treeView.append_column (thisButton.second->viewName,
+				*thisButton.second->columnIcon);
+		}
+		// Add this first frame
+		addNewFrame (true);
+	}
 
-  bool getButtonState(Btn button) {
-    // Get value from the bitset
-    return currentData->buttons.test(button);
-  }
+	bool getButtonState (Btn button) {
+		// Get value from the bitset
+		return currentData->buttons.test (button);
+	}
 
-  void setButtonState(Btn button, bool state) {
-    // If state is true, on, else off
-    return currentData->buttons.set(button, state);
-  }
+	void setButtonState (Btn button, bool state) {
+		// If state is true, on, else off
+		return currentData->buttons.set (button, state);
+	}
 
-  void setCurrentFrame(uint32_t frameNum) {
-    // Must be a frame that has already been written, else, raise error
-    if (frameNum < inputsList.size() && frameNum > -1) {
-      // Set the current frame to this frame
-      // Shared pointer so this can be done
-      currentData = inputsList[frameNum];
-    }
-  }
+	void setCurrentFrame (uint32_t frameNum) {
+		// Must be a frame that has already been written, else, raise error
+		if (frameNum < inputsList.size () && frameNum > -1) {
+			// Set the current frame to this frame
+			// Shared pointer so this can be done
+			currentData = inputsList[frameNum];
+			// Set the current frame to this number
+			currentFrame = frameNum;
+		}
+	}
 
-  void addNewFrame(bool isFirstFrame = false) {
-    if (!isFirstFrame) {
-      // On the first frame, it is already set right
-      currentFrame++;
-    }
-    // Will overwrite previous frame if need be
-    currentData = std::make_shared<ControllerData>();
-    // Add this to the vector
-    inputsList.push_back(currentData);
-    // Add to the table
-    Gtk::TreeModel::Row row = *(controllerListStore->append());
-    row[inputColumns.m_col_id] = 1;
-    row[m_Columns.m_col_name] = "Billy Bob";
-    row[m_Columns.m_col_number] = 10;
-    row[m_Columns.m_col_percentage] = 15;
-  }
+	void addNewFrame (bool isFirstFrame = false) {
+		if (!isFirstFrame) {
+			// On the first frame, it is already set right
+			// This will automatically add at the end even when the
+			// Current frame is not the most recent
+			currentFrame = inputsList.size ();
+		}
+		// Will overwrite previous frame if need be
+		currentData = std::make_shared<ControllerData> ();
+		// Add this to the vector
+		inputsList.push_back (currentData);
+		// Add to the table
+		Gtk::TreeModel::Row row    = *(controllerListStore->append ());
+		row[inputColumns.frameNum] = currentFrame;
+		for (auto const& pixbufData : inputColumns.buttonPixbufs) {
+			// Turn all buttons automatically to off
+			// Dereference pointer to get to it
+			row[*pixbufData.second] = buttonMapping[pixbufData.first].offIcon;
+		}
+	}
 
-  Gtk::TreeView *getTreeview() {
-    // Get pointer of the treeview
-    // The value should be saftely dereferenced once the
-    // Class dies, but I dunno
-    return &treeView;
-  }
+	Gtk::TreeView* getTreeview () {
+		// Get pointer of the treeview
+		// The value should be saftely dereferenced once the
+		// Class dies, but I dunno
+		return &treeView;
+	}
 
-  ~DataProcessing() {}
+	~DataProcessing () { }
 }
