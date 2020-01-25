d63735bc33773c77b62a3114db0530ff42b81e8c
TheGreatRambler <aehart684351@gmail.com>
2020-01-25 00:34:14 -0500

    Phooey, I dunno

--- pc_application/cCC.sh
+++ pc_application/cCC.sh
@@ -4,4 +4,4 @@ sed -i 's|-I/|-IC:/msys2/|g' compile_commands.json
 # Have to separate ALL -I from the rest of the text, vscode bug
 # sed -i 's|-I|-I","|g' compile_commands.json
 # Move the file
-mv compile_commands.json ../compile_commands.json
+# mv compile_commands.json ../compile_commands.json
