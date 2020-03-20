19ce744afede4e2f33bd596927c687aeed4b46a3
TheGreatRambler <aehart684351@gmail.com>
2020-03-20 01:07:42 -0400

    Add project chooser, untested

--- pc_application/source/dataHandling/projectHandler.cpp
+++ pc_application/source/dataHandling/projectHandler.cpp
@@ -1,3 +1,90 @@
 #include "projectHandler.hpp"
 
-ProjectHandler::ProjectHandler() {}
+ProjectHandler::ProjectHandler(DataProcessing* dataProcessingInstance)
+	: wxFrame(NULL, wxID_ANY, "Select Project", wxDefaultPosition, wxDefaultSize) {
+	// Get global settings file
+	getGlobalSettings();
+	dataProcessing = dataProcessingInstance;
+
+	mainSizer = new wxBoxSizer(wxVERTICAL);
+
+	projectList = new wxRichTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxRE_MULTILINE | wxRE_READONLY);
+
+	auto recentProjectsArray = mainSettings["recentProjects"].GetArray();
+
+	wxTextAttr projectNameAttr;
+	projectNameAttr.SetFontUnderlined(true);
+	projectNameAttr.SetFontSize(30);
+	projectNameAttr.SetFontStyle(wxFONTSTYLE_ITALIC);
+	projectNameAttr.SetFontWeight(wxFONTWEIGHT_BOLD);
+	projectNameAttr.SetTextColour(wxColor(mainSettings["ui"]["recentProjectsColors"]["projectName"].GetString()));
+
+	wxTextAttr projectDirectoryAttr;
+	projectDirectoryAttr.SetFontSize(22);
+	projectDirectoryAttr.SetFontStyle(wxFONTSTYLE_NORMAL);
+	projectDirectoryAttr.SetFontWeight(wxFONTWEIGHT_LIGHT);
+	projectDirectoryAttr.SetTextColour(wxColor(mainSettings["ui"]["recentProjectsColors"]["projectDir"].GetString()));
+
+	for(auto const& recentProject : recentProjectsArray) {
+		// Mark as URL and give the directory as the URL
+		projectList->BeginURL(recentProject["projectDirectory"].GetString());
+		projectList->SetDefaultStyle(projectNameAttr);
+		projectList->AppendText(recentProject["projectName"].GetString());
+		projectList->AppendText(" ");
+		projectList->SetDefaultStyle(projectDirectoryAttr);
+		projectList->AppendText(recentProject["projectDirectory"].GetString());
+		projectList->EndURL();
+		projectList->AppendText("\n");
+	}
+
+	wxTextAttr newProjectAttr;
+	newProjectAttr.SetFontSize(35);
+	newProjectAttr.SetFontStyle(wxFONTSTYLE_NORMAL);
+	newProjectAttr.SetFontWeight(wxFONTWEIGHT_LIGHT);
+	newProjectAttr.SetTextColour(wxColor(mainSettings["ui"]["recentProjectsColors"]["newProject"].GetString()));
+
+	projectList->BeginURL("newProject");
+	projectList->SetDefaultStyle(newProjectAttr);
+	projectList->AppendText("Create New Project");
+	projectList->EndURL();
+
+	projectList->Bind(wxEVT_TEXT_URL, &ProjectHandler::onClickProject, this);
+
+	mainSizer->Add(projectList, 0, wxEXPAND | wxALL);
+
+	// Print create project input
+
+	SetSizer(mainSizer);
+	mainSizer->SetSizeHints(this);
+	Layout();
+	Fit();
+	Center(wxBOTH);
+}
+
+void ProjectHandler::getGlobalSettings() {
+	std::ifstream settingsFile("../mainSettings.json");
+	std::string content((std::istreambuf_iterator<char>(settingsFile)), (std::istreambuf_iterator<char>()));
+	// Allow comments in JSON
+	mainSettings.Parse<rapidjson::kParseCommentsFlag>(content.c_str());
+}
+
+void ProjectHandler::onClickProject(wxTextUrlEvent& event) {
+	wxString selectedProject = event.GetString();
+	if(selectedProject == "newProject") {
+		// Open up a new project dialog
+		wxDirDialog dlg(NULL, "Choose Project Directory", "", wxDD_DEFAULT_STYLE);
+		if(dlg.ShowModal() == wxID_OK) {
+			// Directory chosen
+			projectDir.Open(dlg.GetPath());
+		}
+	} else {
+		// It's a project folder
+		projectDir.Open(selectedProject);
+		// Only difference between new project and existing project is
+		// DataProcessing is automatically filled with the existing data
+	}
+}
+
+void ProjectHandler::saveProject() {
+	// Save each set of data one by one
+}
--- pc_application/source/dataHandling/projectHandler.hpp
+++ pc_application/source/dataHandling/projectHandler.hpp
@@ -1,13 +1,40 @@
 #pragma once
 
-class ProjectHandler {
+#include <fstream>
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+#include <wx/dir.h>
+#include <wx/dirdlg.h>
+#include <wx/grid.h>
+#include <wx/richtext/richtextctrl.h>
+#include <wx/wx.h>
+
+#include "dataProcessing.hpp"
+
+class ProjectHandler : public wxFrame {
 private:
 	// Add directory of project, project name, various other info
 	// This will handle opening a directory chooser at the beginning of each session
 	// https://docs.wxwidgets.org/3.0/classwx_dir_dialog.html
 	// Probably having a list of the recent projects as well
 	// Store recent projects in mainSettings.json
+	wxBoxSizer* mainSizer;
+	wxRichTextCtrl* projectList;
+
+	DataProcessing* dataProcessing;
+
+	wxDir projectDir;
+
+	// Main settings variable
+	rapidjson::Document mainSettings;
+
+	void getGlobalSettings();
+
+	void onClickProject(wxTextUrlEvent& event);
 
 public:
-	ProjectHandler();
+	ProjectHandler(DataProcessing* dataProcessingInstance);
+
+	void saveProject();
 };
